9566eaa39d9d22873c725dae8183a21f69d9ec52
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-10 12:47:41 +0530

    add router

---
+++ restaurant-reservation-server/src/router.ts
@@ -0,0 +1,10 @@
+/**
+ * @created 10/09/2021 - 12:46
+ * @project Online-Restaurant-Reservation-System-ORRS
+ * @author  Shevan
+ * @file    router
+ */
+
+import { Router } from 'express';
+
+export const router = Router();
