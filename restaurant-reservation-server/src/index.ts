a2a5facb8287462fa9820987d581bb5595b40109
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-11 10:36:13 +0530

    add index

---
+++ restaurant-reservation-server/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @created 10/09/2021 - 13:26
+ * @project Online-Restaurant-Reservation-System-ORRS
+ * @author  Shevan
+ * @file    index
+ */
+
+import { app } from '@src/app';
+import { logger } from '@src/util/logger';
+
+import { PORT } from './config';
+
+const startServer = async () => {
+  // start listening
+  app.listen(PORT, async () => {
+    logger.info(`app started on port ${PORT}`);
+  });
+};
+
+startServer();
