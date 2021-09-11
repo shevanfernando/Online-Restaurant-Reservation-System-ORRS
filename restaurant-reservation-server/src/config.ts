12582f38b927838bdaeeba4830233ccd8f69b796
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-11 10:36:39 +0530

    add config

--- restaurant-reservation-server/src/config.ts
+++ restaurant-reservation-server/src/config.ts
@@ -9,16 +9,5 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const ENV = process.env.NODE_ENV === undefined ? 'development' : process.env.NODE_ENV;
-export const PORT = process.env.APP_PORT === undefined ? 3000 : process.env.APP_PORT;
-export const APP_URL = process.env.APP_URL === undefined ? 'http://localhost' : process.env.APP_URL;
-export const DATABASE_URL = () => {
-  if (ENV === 'development') {
-    return process.env.DB_URL;
-  }
-
-  return process.env.DB_TEST_URL;
-};
-export const API_PREFIX = '/api';
-export const TOKEN_SECRET = process.env.TOKEN_SECRET === undefined ? 'Pas@wOrd' : process.env.TOKEN_SECRET;
-export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME === undefined ? '1h' : process.env.TOKEN_EXPIRE_TIME;
+export const ENV = process.env.NODE_ENV || 'development';
+export const PORT = process.env.APP_PORT || 30000;
