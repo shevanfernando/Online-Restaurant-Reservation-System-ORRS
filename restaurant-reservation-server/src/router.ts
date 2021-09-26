26b9f78384e7d21b7160d5a862b9f6d6f466e926
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-26 16:04:33 +0530

    implement user model

--- restaurant-reservation-server/src/router.ts
+++ restaurant-reservation-server/src/router.ts
@@ -5,6 +5,12 @@
  * @file    router
  */
 
+import userRouter from '@src/api/user/user.router';
+import { API_PREFIX } from '@src/config';
 import { Router } from 'express';
 
-export const router = Router();
+const router = Router();
+
+router.use(`${API_PREFIX}/user`, userRouter);
+
+export default router;
