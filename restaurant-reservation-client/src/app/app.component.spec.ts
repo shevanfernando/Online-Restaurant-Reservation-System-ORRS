243f5088d4cbac6925769df00a202208e8c8f52f
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-10-03 13:13:04 +0530

    fixed client side test failure

--- restaurant-reservation-client/src/app/app.component.spec.ts
+++ restaurant-reservation-client/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('restaurant-reservation-client app is running!');
+    expect(compiled.querySelector('p')?.textContent).toContain('restaurant-reservation-client app is running!');
   });
 });
