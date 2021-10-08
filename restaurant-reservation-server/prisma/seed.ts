592dbcbb2b277bdce4421a57e834466e72266326
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-08 16:39:16 +0530

    update seeder

--- restaurant-reservation-server/prisma/seed.ts
+++ restaurant-reservation-server/prisma/seed.ts
@@ -9,8 +9,9 @@ import { PrismaClient } from '@prisma/client';
 import config from '../src/config';
 import { PasswordCrypto } from '@util/PasswordCrypto';
 
-import { CustomerDataList, StaffDataList } from './dummy-data/UserData';
+import userData from './dummy-data/UserData';
 import { logger } from '@util/logger';
+import sequenceGenerator from '@util/IdSequenceGenerator';
 
 const prisma = new PrismaClient({
   datasources: {
@@ -24,25 +25,34 @@ async function main() {
   logger.info('Start seeding...');
 
   // Add Staff Members
-  for (const s of StaffDataList) {
+  for (const s of userData.StaffDataList) {
     if ((await prisma.person.findUnique({ where: { email: s.Person.create.email } })) === null) {
+      const id = await sequenceGenerator.idGenerator('STF_', 'staff');
       await new PasswordCrypto()
         .encrypt(s.User.create.password)
         .then((res) => (s.User.create.password = res))
         .catch((err) => logger.error(err));
-      const staff = await prisma.staff.create({ data: s });
+      const staff = await prisma.staff.create({
+        data: {
+          staffId: id,
+          staffType: s.staffType,
+          Person: s.Person,
+          User: s.User,
+        },
+      });
       logger.info(`Created staff member with id: ${staff.staffId}`);
     }
   }
 
   // Add Customers
-  for (const c of CustomerDataList) {
+  for (const c of userData.CustomerDataList) {
     if ((await prisma.person.findUnique({ where: { email: c.Person.create.email } })) === null) {
+      const id = await sequenceGenerator.idGenerator('CUS_', 'customer');
       await new PasswordCrypto()
         .encrypt(c.User.create.password)
         .then((res) => (c.User.create.password = res))
         .catch((err) => logger.error(err));
-      const customer = await prisma.customer.create({ data: c });
+      const customer = await prisma.customer.create({ data: { customerId: id, Person: c.Person, User: c.User } });
       logger.info(`Created customer with id: ${customer.customerId}`);
     }
   }
