e98d3af5ae22823ce9519de13759a4790a9924d4
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-10 19:47:43 +0530

    add food dummy data

---
+++ restaurant-reservation-server/prisma/dummy-data/FoodData.ts
@@ -0,0 +1,11 @@
+/**
+ * @created 08/10/2021 - 07:56
+ * @project settings.json
+ * @author  Shevan
+ * @file    FoodData
+ */
+const FoodDataList = [
+  { create: { name: 'Fried Rice', description: 'Lorem ipsum', price: 320.0, foodType: 'MAIN_DISH' } },
+];
+
+export default FoodDataList;
