419977c2b229f55db981954c148bbd90bf18401c
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-16 11:34:41 +0530

    add feedback router

--- restaurant-reservation-server/src/router.ts
+++ restaurant-reservation-server/src/router.ts
@@ -8,11 +8,15 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import config from '@src/config';
 import userRouter from '@api/user/user.router';
+
+import feedbackRouter from '@api/feedback/feedback.router';
 import upload from '@util/image-saver';
 
 const router: Router = Router();
 
 router.use(`${config.API_PREFIX}/user`, userRouter);
+
+router.use(`${config.API_PREFIX}/feedback`, feedbackRouter);
 
 router.post(
   `${config.API_PREFIX}/test-file-upload`,
