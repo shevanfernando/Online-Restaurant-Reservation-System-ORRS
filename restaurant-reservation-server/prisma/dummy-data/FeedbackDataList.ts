0ad5a026ae3a0b6f505cde49ae092756cafa7bdb
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-16 11:35:57 +0530

    implement feedback dummy data list

---
+++ restaurant-reservation-server/prisma/dummy-data/FeedbackDataList.ts
@@ -0,0 +1,29 @@
+/**
+ * @created 16/10/2021 - 11:04
+ * @project settings.json
+ * @author  Shevan
+ * @file    FeedbackData
+ */
+
+import { Experiance, FeedbackType } from '@prisma/client';
+
+export const FeedbackDataList = [
+  {
+    level: Experiance.GOOD,
+    feedback:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    type: FeedbackType.SUGGESTION,
+  },
+  {
+    level: Experiance.POOR,
+    feedback:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    type: FeedbackType.BUG,
+  },
+  {
+    level: Experiance.AVERAGE,
+    feedback:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    type: FeedbackType.OTHERS,
+  },
+];
