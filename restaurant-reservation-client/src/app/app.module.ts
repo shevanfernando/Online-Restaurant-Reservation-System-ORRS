b4cfc76ac1fc76f9e7ebcddc4f7162f749e6b5ec
bimsara <bimsara.pradeep12@gmail.com>
2021-10-16 17:34:34 +0530

    implement feedback ui

--- restaurant-reservation-client/src/app/app.module.ts
+++ restaurant-reservation-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
