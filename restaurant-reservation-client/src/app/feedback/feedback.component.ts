4b684e1b8b58c86ed316380d170087334f95aab2
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-18 12:29:12 +0530

    implement reactive from and add feedback service

--- restaurant-reservation-client/src/app/feedback/feedback.component.ts
+++ restaurant-reservation-client/src/app/feedback/feedback.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FeedbackService } from '../services/feedback.service';
+import alert from 'sweetalert2';
+
+export type FeedbackDTO = {
+  level: {
+    POOR: 'POOR';
+    AVERAGE: 'AVERAGE';
+    GOOD: 'GOOD';
+  };
+  feedback: string;
+  type: {
+    BUG: 'BUG';
+    SUGGESTION: 'SUGGESTION';
+    OTHER: 'OTHER';
+  };
+};
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.scss']
+  styleUrls: ['./feedback.component.scss'],
 })
-export class FeedbackComponent implements OnInit {
+export class FeedbackComponent {
+  feedbackForm = new FormGroup({
+    level: new FormControl('', Validators.required),
+    feedback: new FormControl(''),
+    type: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(private feedbackService: FeedbackService) {}
 
-  ngOnInit(): void {
+  onFeedbackSend() {
+    if (this.feedbackForm.valid) {
+      this.feedbackService
+        .createNewFeedback(this.feedbackForm.value)
+        .then((res) => {
+          alert
+            .fire('Thanks for your Feedback!', undefined, 'success')
+            .then((alertResult) => {
+              console.log(alertResult);
+            });
+        })
+        .catch((err) => console.log(err));
+    }
   }
-
 }
