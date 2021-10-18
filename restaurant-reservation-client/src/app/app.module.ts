9bcf59cc4e31e06e4a31b867533f2ff27e0bcb53
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-18 12:29:30 +0530

    add HttpClientModule

--- restaurant-reservation-client/src/app/app.module.ts
+++ restaurant-reservation-client/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FeedbackComponent
-  ],
+  declarations: [AppComponent, FeedbackComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
