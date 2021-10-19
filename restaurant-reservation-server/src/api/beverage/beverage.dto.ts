11de5559e2c8e2d15092bb71fb6ce282b91ccc93
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-19 17:16:35 +0530

    implement beverage dto

---
+++ restaurant-reservation-server/src/api/beverage/beverage.dto.ts
@@ -0,0 +1,41 @@
+/**
+ * @created 19/10/2021
+ * @project Online-Restaurant-Reservation-System-ORRS
+ * @author  Shevan
+ * @file    beverage.dto
+ */
+
+import Joi, { ValidationError } from 'joi';
+import { BeverageType } from '@prisma/client';
+import { VictualDTO, victualDTOObject } from '@api/shared/victual.dto';
+
+export type BeverageDTO = {
+  beverageType: BeverageType;
+  victual: VictualDTO;
+};
+
+export const beverageDTOObject = Joi.object({
+  beverageType: Joi.valid(
+    BeverageType.BEER,
+    BeverageType.BRANDY,
+    BeverageType.RUM,
+    BeverageType.VINE,
+    BeverageType.VODKA,
+    BeverageType.REFRESHING,
+    BeverageType.NOURISHING,
+    BeverageType.STIMULATING,
+    BeverageType.WHISKY
+  )
+    .required()
+    .messages({
+      'any.only': `Beverage Type is allowed only, {#valids}`,
+      'any.required': 'Beverage Type is required',
+    }),
+  victual: victualDTOObject,
+}).required();
+
+export default {
+  validate: (data: unknown): { error?: ValidationError; value: BeverageDTO } => {
+    return beverageDTOObject.validate(data, { stripUnknown: true, abortEarly: false });
+  },
+};
