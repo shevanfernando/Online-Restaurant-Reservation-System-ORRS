96bc821efe68ba82da8dbb81b69bc4eab92e32b9
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-19 20:58:31 +0530

    remove reactive form and feedback component

--- restaurant-reservation-client/src/app/app.module.ts
+++ restaurant-reservation-client/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, FeedbackComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
