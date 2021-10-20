da7e4e6198cafbf8e49cc0a024fe1f5a91231845
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-20 07:52:55 +0530

    develop login

--- restaurant-reservation-client/src/app/pages/login/login.component.ts
+++ restaurant-reservation-client/src/app/pages/login/login.component.ts
@@ -15,6 +15,10 @@ declare function sign_in(): void;
 export class LoginComponent {
   myScriptElement?: HTMLScriptElement;
 
+  errorMessages: string | undefined;
+
+  formSubmitInvalid: boolean = false;
+
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
@@ -36,9 +40,30 @@ export class LoginComponent {
 
   onClickLogin() {
     if (this.loginForm.valid) {
-      this.authService.signIn(this.loginForm.value).subscribe((next) => {
-        console.log('Save');
-      });
+      this.authService.signIn(this.loginForm.value).subscribe(
+        (next) => {
+          //TODO: Add redirect Path
+          console.log('Save');
+        },
+        (err) => {
+          this.errorMessages = err.error.message;
+        }
+      );
+    } else {
+      this.formSubmitInvalid = true;
+      if (
+        this.loginForm.controls.username.invalid &&
+        this.loginForm.controls.password.invalid
+      ) {
+        this.errorMessages = 'All fields are required';
+      } else if (this.loginForm.controls.username.invalid)
+        this.errorMessages = 'Username is required';
+      else if (this.loginForm.controls.password.invalid)
+        this.errorMessages = 'Password is required';
     }
+  }
+
+  onInputFieldsChnage() {
+    this.errorMessages = undefined;
   }
 }
