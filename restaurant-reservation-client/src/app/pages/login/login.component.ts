005ca6c6219fc4f926796c1152c18a940a8c6819
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-20 09:20:49 +0530

    develop customer registration

--- restaurant-reservation-client/src/app/pages/login/login.component.ts
+++ restaurant-reservation-client/src/app/pages/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../core/auth/auth.service';
+import { UserService, UserType } from '../../services/user.service';
+import alert from 'sweetalert2';
 
 declare function sign_up(): void;
 
@@ -24,24 +31,54 @@ export class LoginComponent {
     password: new FormControl('', [Validators.required]),
   });
 
-  constructor(private router: Router, private authService: AuthService) {
+  registrationForm = new FormGroup(
+    {
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phoneNumber: new FormControl('', [
+        Validators.pattern(
+          /^[\\+]?[(]?[0-9]{3}[)]?[-\s\\.]?[0-9]{3}[-\s\\.]?[0-9]{4}$/
+        ),
+      ]),
+      nic: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/m),
+      ]),
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      userType: new FormControl(UserType.CUSTOMER, [Validators.required]),
+    },
+    {
+      validators: this.passwordConfirm,
+    }
+  );
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService
+  ) {
     this.myScriptElement = document.createElement('script');
     this.myScriptElement.src = '../../assets/appJs/app.js';
     document.body.appendChild(this.myScriptElement);
   }
 
   signUp() {
+    this.errorMessages = undefined;
     sign_up();
   }
 
   signIn() {
+    this.errorMessages = undefined;
     sign_in();
   }
 
   onClickLogin() {
     if (this.loginForm.valid) {
       this.authService.signIn(this.loginForm.value).subscribe(
-        (next) => {
+        () => {
           //TODO: Add redirect Path
           console.log('Save');
         },
@@ -63,7 +100,73 @@ export class LoginComponent {
     }
   }
 
-  onInputFieldsChnage() {
+  onClickSignUp() {
+    if (this.registrationForm.valid) {
+      this.userService.signUp(this.registrationForm.value).subscribe(
+        () => {
+          alert
+            .fire('Customer registration successful!', undefined, 'success')
+            .then((alertResult) => {
+              this.router.navigateByUrl('/login');
+            });
+        },
+        (err) => {
+          alert.fire('Customer registration failed!', undefined, 'error');
+          this.errorMessages = err.message;
+        }
+      );
+    } else {
+      if (
+        this.registrationForm.controls.firstName.invalid &&
+        this.registrationForm.controls.email.invalid &&
+        this.registrationForm.controls.phoneNumber.invalid &&
+        this.registrationForm.controls.nic.invalid &&
+        this.registrationForm.controls.username.invalid &&
+        this.registrationForm.controls.password.invalid &&
+        this.registrationForm.controls.confirmPassword.invalid
+      ) {
+        this.errorMessages = 'All Fields are required';
+      } else {
+        if (this.registrationForm.controls.firstName.invalid) {
+          this.errorMessages = 'First Name is required';
+        }
+        if (this.registrationForm.controls.email.invalid) {
+          if (this.registrationForm.controls.firstName.errors?.email)
+            this.errorMessages = 'Email is not valid.';
+
+          this.errorMessages = 'Email is required';
+        }
+        if (this.registrationForm.controls.phoneNumber.invalid) {
+          this.errorMessages = 'Phone Number is not valid.';
+        }
+        if (this.registrationForm.controls.nic.invalid) {
+          if (this.registrationForm.controls.nic.errors?.pattern)
+            this.errorMessages = 'NIC is not valid.';
+          this.errorMessages = 'NIC is required.';
+        }
+        if (this.registrationForm.controls.username.invalid) {
+          this.errorMessages = 'Username is required.';
+        }
+        if (this.registrationForm.controls.password.invalid) {
+          this.errorMessages = 'Password is required.';
+        }
+        if (this.registrationForm.controls.confirmPassword.invalid) {
+          this.errorMessages = 'Confirm Password is required.';
+        }
+      }
+    }
+  }
+
+  onInputFieldsChange() {
     this.errorMessages = undefined;
+  }
+
+  passwordConfirm(form: AbstractControl): { invalid: boolean } {
+    if (form.get('password')?.value === form.get('confirmPassword')?.value) {
+      return { invalid: false };
+    } else {
+      this.errorMessages = 'Password and Confirm password are not matched';
+      return { invalid: true };
+    }
   }
 }
---
+++ restaurant-reservation-client/src/app/services/user.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export enum StaffType {
+  RECEPTIONIST = 'RECEPTIONIST',
+  WAITER = 'WAITER',
+  ADMIN = 'ADMIN',
+  CHEF = 'CHEF',
+}
+
+export enum UserType {
+  CUSTOMER = 'CUSTOMER',
+  STAFF = 'STAFF',
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly _API: string;
+
+  constructor(private http: HttpClient) {
+    this._API = `${environment.API}/user`;
+  }
+
+  signIn(data: { username: string; password: string }): Observable<string> {
+    return this.http.post<string>(`${this._API}/login`, data);
+  }
+
+  signUp(data: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    nic: string;
+    username: string;
+    password: string;
+    userType: UserType;
+    staffType?: StaffType;
+  }): Observable<void> {
+    if (data.userType === UserType.CUSTOMER) {
+      return this.http.post<void>(`${this._API}/customer-registration`, {
+        person: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+          phoneNumber: data.phoneNumber,
+          nic: data.nic,
+        },
+        user: {
+          username: data.username,
+          password: data.password,
+          userType: data.userType,
+        },
+      });
+    } else {
+      return this.http.post<void>(`${this._API}/staff-registration`, {
+        person: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+          phoneNumber: data.phoneNumber,
+          nic: data.nic,
+        },
+        user: {
+          username: data.username,
+          password: data.password,
+          userType: data.userType,
+        },
+        staffType: data.staffType,
+      });
+    }
+  }
+}
