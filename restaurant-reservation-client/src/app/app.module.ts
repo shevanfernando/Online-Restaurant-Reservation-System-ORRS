240d00c6024582276bcc87607ba82c85e0b1f3ff
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-20 07:52:14 +0530

    add http interceptors

--- restaurant-reservation-client/src/app/app.module.ts
+++ restaurant-reservation-client/src/app/app.module.ts
@@ -3,12 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { JwtInterceptor } from './core/auth/jwt.interceptor';
+import { UnauthorizedInterceptor } from './core/auth/unauthorized.interceptor';
+import { RequestMonitorInterceptor } from './core/request-monitor.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UnauthorizedInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestMonitorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
