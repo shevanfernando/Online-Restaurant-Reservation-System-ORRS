7cc22c7aa983eb3bf8b4266974a8e7540847511d
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-20 07:51:59 +0530

    add default route path

--- restaurant-reservation-client/src/app/app-routing.module.ts
+++ restaurant-reservation-client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { E404Component } from './pages/errors/e404/e404.component';
 
 const routes: Routes = [
   {
@@ -7,6 +8,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/pages.module').then((m) => m.PagesModule),
   },
+  { path: '**', component: E404Component },
 ];
 
 @NgModule({
