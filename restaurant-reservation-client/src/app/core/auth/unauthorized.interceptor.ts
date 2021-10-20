d6c240354efe7a254a221af0900933a383c4e5f3
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-20 07:51:32 +0530

    add unauthorized interceptor

---
+++ restaurant-reservation-client/src/app/core/auth/unauthorized.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class UnauthorizedInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          this.authService.signOut();
+          this.router.navigateByUrl('/login');
+        }
+
+        const error = err.error.message || err.statusText || err;
+        return throwError(error);
+      })
+    );
+  }
+}
