e24e2b459cb6428345d9fddba912c78f9a6b6dec
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-11-19 10:32:34 +0530

    add image save router

--- restaurant-reservation-server/src/api/food/food.router.ts
+++ restaurant-reservation-server/src/api/food/food.router.ts
@@ -11,27 +11,28 @@ import { foodFilterDTOObject } from '@api/food/food-filter.dto';
 import { HttpValidationError } from '@lib/HttpValidationError';
 import { foodDTOObject } from '@api/food/food.dto';
 import AuthGuard, { Roles } from '@middlewares/auth-guard';
+import { foodUpdateDTOObject } from '@api/food/food-update.dto';
+import upload from '@util/image-saver';
+import { imageSaveDTOObject } from '@api/shared/victual/image-save.dto';
+import victualService from '@api/shared/victual/victual.service';
+import { itemDeleteDTO } from '@api/shared/item-delete.dto';
 
 const router = Router();
+// TODO: implement image save feature and image fetch feature
+router.post('/add', AuthGuard([Roles.ADMIN, Roles.CHEF]), async (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = foodDTOObject.validate(req.body);
 
-router.post(
-  '/add-food',
-  AuthGuard([Roles.ADMIN, Roles.CHEF]),
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { error, value } = foodDTOObject.validate(req.body);
-
-    if (error) {
-      return next(new HttpValidationError(error));
-    }
-
-    foodService
-      .addFood(value)
-      .then((result) => res.status(200).json(result))
-      .catch((err) => next(err));
+  if (error) {
+    return next(new HttpValidationError(error));
   }
-);
 
-router.get('/get-food', async (req: Request, res: Response, next: NextFunction) => {
+  foodService
+    .addFood(value)
+    .then((result) => res.status(200).json(result))
+    .catch((err) => next(err));
+});
+
+router.get('/get', async (req: Request, res: Response, next: NextFunction) => {
   const { error, value } = foodFilterDTOObject.validate(req.query);
   if (error) {
     return next(new HttpValidationError(error));
@@ -41,5 +42,53 @@ router.get('/get-food', async (req: Request, res: Response, next: NextFunction) 
     .then((result) => res.status(200).json(result))
     .catch((err) => next(err));
 });
+
+router.put('/update', AuthGuard([Roles.ADMIN, Roles.CHEF]), async (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = foodUpdateDTOObject.validate(req.body);
+
+  if (error) {
+    return next(new HttpValidationError(error));
+  }
+
+  foodService
+    .updateFood(value)
+    .then((result) => res.status(200).json(result))
+    .catch((err) => next(err));
+});
+
+router.delete('/delete/:id', AuthGuard([Roles.ADMIN]), async (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = itemDeleteDTO.validate(req.params);
+
+  if (error) {
+    return next(new HttpValidationError(error));
+  }
+
+  foodService
+    .deleteFood(value)
+    .then((result) => res.status(200).json(result))
+    .catch((err) => next(err));
+});
+
+router.post(
+  '/add-picture',
+  AuthGuard([Roles.ADMIN, Roles.CHEF]),
+  upload,
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    const { error, value } = imageSaveDTOObject.validate({
+      imagePath: req.file?.filename,
+      victualId: req.body.victualId,
+    });
+
+    if (error) {
+      return next(new HttpValidationError(error));
+    }
+
+    victualService
+      .addImage(value, 'Food')
+      .then((result) => res.status(200).json(result))
+      .catch((err) => next(err));
+  }
+);
 
 export default router;
