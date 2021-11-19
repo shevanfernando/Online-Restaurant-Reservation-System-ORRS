d7c2839c7bd877acf669f4bf738ee61ac20f2e01
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-11-19 10:32:49 +0530

    implement food update dto

---
+++ restaurant-reservation-server/src/api/food/food-update.dto.ts
@@ -0,0 +1,38 @@
+/**
+ * @created 17/11/2021
+ * @project Online-Restaurant-Reservation-System-ORRS
+ * @author  Shevan
+ * @file    food-update.dto.ts
+ */
+
+import Joi, { ValidationError } from 'joi';
+import { FoodType } from '@prisma/client';
+import { VictualDTO, victualDTOObject } from '@api/shared/victual/victual.dto';
+
+export type FoodUpdateDTO = {
+  foodId: string;
+  foodType: FoodType;
+  victual: VictualDTO;
+};
+
+export const foodUpdateDTOObject = Joi.object({
+  foodId: Joi.string().length(12).required().messages({
+    'string.base': 'Food Id should be a type of "string"',
+    'string.empty': 'Food Id cannot be an empty field',
+    'string.length': `Food Id should have a {#limit} characters.`,
+    'any.required': 'Food Id is required',
+  }),
+  foodType: Joi.valid(FoodType.DESSERTS, FoodType.MAIN_COURSES, FoodType.SIDE_DISHES, FoodType.STARTERS)
+    .required()
+    .messages({
+      'any.only': `Food Type is allowed only, {#valids}`,
+      'any.required': 'Food Type is required',
+    }),
+  victual: victualDTOObject,
+}).required();
+
+export default {
+  validate: (data: unknown): { error?: ValidationError; value: FoodUpdateDTO } => {
+    return foodUpdateDTOObject.validate(data, { stripUnknown: true, abortEarly: false });
+  },
+};
