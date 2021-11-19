1322876690a55d42bbb9df8a9b4dd51756fbcbd3
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-11-19 10:30:42 +0530

    add feedback component router

--- restaurant-reservation-client/src/app/pages/pages-routing.module.ts
+++ restaurant-reservation-client/src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 const routes: Routes = [
   {
@@ -8,11 +9,16 @@ const routes: Routes = [
     component: LoginComponent,
   },
   {
+    path: 'feedback',
+    component: FeedbackComponent,
+  },
+  {
     path: 'error',
     loadChildren: () =>
       import('./errors/errors.module').then((m) => m.ErrorsModule),
   },
 ];
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
