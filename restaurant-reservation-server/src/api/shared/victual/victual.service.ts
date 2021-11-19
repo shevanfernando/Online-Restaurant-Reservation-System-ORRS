edc156b9f58b6d24c3d7baf0733a47d756888f5e
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-11-19 10:29:15 +0530

    implement victual service

---
+++ restaurant-reservation-server/src/api/shared/victual/victual.service.ts
@@ -0,0 +1,30 @@
+/**
+ * @created 18/11/2021
+ * @project Online-Restaurant-Reservation-System-ORRS
+ * @author  Shevan
+ * @file    victual.service
+ */
+import { Prisma, PrismaClient, Victual } from '@prisma/client';
+import { ImageSaveDTO } from '@api/shared/victual/image-save.dto';
+import { HttpError } from '@lib/HttpError';
+
+const prisma = new PrismaClient();
+
+const addImage = async (data: ImageSaveDTO, field: string): Promise<Prisma.Prisma__VictualClient<Victual> | void> => {
+  return prisma.victual
+    .update({
+      where: { victualId: data.victualId },
+      data: { imagePath: data.imagePath },
+    })
+    .catch((err) => {
+      const { code } = err;
+      if (code === 'P2025') {
+        throw new HttpError(404, `Can't find any ${field} item using this victual id.`);
+      }
+      return err;
+    });
+};
+
+export default {
+  addImage,
+};
